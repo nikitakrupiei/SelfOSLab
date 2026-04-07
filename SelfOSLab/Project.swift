b8565a495d48b66d4e4fc99bfdc8c2c22eec6faf
Yehor Krupiei <yehorkeupiei@icloud.com>
2026-04-07 19:03:24 +0300

    first commit

---
+++ SelfOSLab/Project.swift
@@ -0,0 +1,63 @@
+import ProjectDescription
+
+let project = Project(
+    name: "SelfOSLab",
+    packages: [
+        .local(path: "../Platform")
+    ],
+    targets: [
+        .target(
+            name: "SelfOSLab",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "com.nikitakrupiei.selfos",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            buildableFolders: [
+                "SelfOSLab/Sources",
+                "SelfOSLab/Resources",
+            ],
+            dependencies: [
+                .package(product: "Platform")
+            ]
+        ),
+        .target(
+            name: "SelfOSLab-Dev",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "com.nikitakrupiei.selfos.dev",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            buildableFolders: [
+                "SelfOSLab/Sources",
+                "SelfOSLab/Resources",
+            ],
+            dependencies: [
+                .package(product: "Platform")
+            ]
+        ),
+        .target(
+            name: "SelfOSLabTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "com.nikitakrupiei.selfos.dev.tests",
+            infoPlist: .default,
+            buildableFolders: [
+                "SelfOSLab/Tests"
+            ],
+            dependencies: [.target(name: "SelfOSLab-Dev")]
+        ),
+    ]
+)
---
+++ SelfOSLab/SelfOSLab/Sources/SelfOSApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct SelfOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
