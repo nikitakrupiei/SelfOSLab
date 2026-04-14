22afaf15792d77b28ab3ca95fe59da1d04b775e4
Nikita Krupiei <nikitakrupiei@gmail.com>
2026-04-14 18:47:01 +0300

    Jenkins setup

--- SelfOSLab/SelfOSLab/Sources/ContentView.swift
+++ SelfOSLab/SelfOSLab/Sources/ContentView.swift
@@ -1,10 +1,11 @@
 import SwiftUI
+import Platform
 
 public struct ContentView: View {
     public init() {}
 
     public var body: some View {
-        Text("Hello, World!")
+        Text(someString)
             .padding()
     }
 }
